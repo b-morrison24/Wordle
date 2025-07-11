3fe77c95d4d391bd6d0ccace598ccc739ba3ce5d
bmorrison88 <b.mo94@yahoo.com>
2025-07-11 09:39:11 -0500

    Added rest of keys to keyboard

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -4,22 +4,27 @@ export default function Keyboard({onKeyClicked}) {
     // On click, value of KeyButton is passed to guess
     // Read from an array of objects (id, value, className) possible keys, map values to each KeyButton component
     const keyValues = [
-        {id: "Q", value: "Q", classValue: "row_1"},
-        {id: "W", value: "W", classValue: "row_1"},
-        {id: "E", value: "E", classValue: "row_1"},
-        {id: "Backspace", value: "Backspace", classValue: "row_1"},
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
     ]
 
     return (
         <div className="keyboard_container">
             {
-                keyValues.map((keyValue) => {
+                keyValues.map((row, rowIndex) => {
                     return (
-                        <Key
-                            key={keyValue.id}
-                            value={keyValue.value}
-                            onClick={() => onKeyClicked(keyValue.value)}
-                        />
+                        <div className={`row ${rowIndex}`}>
+                            {row.map((keyValue) => {
+                                return (
+                                    <Key
+                                        key={keyValue}
+                                        value={keyValue}
+                                        onClick={() => onKeyClicked(keyValue)}
+                                    />
+                                )
+                            })}
+                        </div>
                     )
                 })
             }
